62ae75da9b898545798300141ee10d2d720bfd54
Memnoc <smartdroidesign@gmail.com>
2018-11-09 08:51:30 +0000

    renamed MainActivity.java to CreateMessageActivity.java renamed activity_main.xml to activity_create_message.xml

--- app/src/main/java/com/smartdroidesign/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/smartdroidesign/messenger/CreateMessageActivity.java
@@ -3,11 +3,11 @@ package com.smartdroidesign.messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_message);
     }
 }
