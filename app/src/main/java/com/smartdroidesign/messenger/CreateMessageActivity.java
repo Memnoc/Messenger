9be969ea7296b846847e0f3186884b334daa6d29
Memnoc <smartdroidesign@gmail.com>
2018-11-12 10:36:18 +0000

    implemented full explicit intent implemented full implicit intent

--- app/src/main/java/com/smartdroidesign/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/smartdroidesign/messenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -14,7 +15,15 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        EditText messageView = findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        // Explicit intent -> send an intent to a specific activity
+//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+//        intent.putExtra("message", messageText);
+        // Implicit intent -> send an intent that contains an actions that can be performed by suitable apps
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/smartdroidesign/messenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/smartdroidesign/messenger/ReceiveMessageActivity.java
@@ -1,13 +1,23 @@
 package com.smartdroidesign.messenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+
+        Intent intent = getIntent();
+        intent.getStringExtra("message");
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = findViewById(R.id.message);
+        messageView.setText(messageText);
     }
 }
